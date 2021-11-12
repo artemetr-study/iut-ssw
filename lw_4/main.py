0750abb77c7664e331ee1288a8eeea5f62f8dfbb
artemetr <my@artemetr.ru>
2021-11-12 15:47:14 +0500

    Добавил инпут

--- lw_4/main.py
+++ lw_4/main.py
@@ -1,4 +1,4 @@
 from lw_4.reverse_polish_notation import ReversePolishNotation
 
 if __name__ == '__main__':
-    print(ReversePolishNotation.from_expression('((l-k+h)/(g-h+k)*4)/p=d-2*(a+b–d /c*d )'))
+    print(ReversePolishNotation.from_expression(input('Input expression: ')))
