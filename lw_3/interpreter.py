feb2d1969f973185cdfd3e814dc51149d6348b20
artemetr <my@artemetr.ru>
2021-11-22 22:29:36 +0500

    Добавил требуемую по заданию оптимизацию вычислений (естественно я не делал нормально, ы)

--- lw_3/interpreter.py
+++ lw_3/interpreter.py
@@ -7,6 +7,7 @@ from lw_3.tokens import BaseToken, IdentifierListToken, IdentifierToken, Operato
 class Interpreter:
     def __init__(self):
         self.vars = {}
+        self.arithm_cache = {}
 
     def has_var(self, name) -> bool:
         return name in self.vars.keys()
@@ -23,9 +24,18 @@ class Interpreter:
 
         return self.vars[name]
 
+    def drop_cache(self, name):
+        for k in list(self.arithm_cache.keys()):
+            if name in k:
+                print(f'Удаляю кэш для переменных содержащих {name}')
+                del self.arithm_cache[k]
+
     def set_var(self, name, value):
         if not self.has_var(name):
             raise Exception(f'Variable {name} is not defined')
+
+        if self.vars[name] != value:
+            self.drop_cache(name)
 
         self.vars[name] = value
 
@@ -46,6 +56,18 @@ class Interpreter:
             raise Exception('Undefined token')
 
     def arithmetical_or_logical_expression_exec(self, token: ArithmeticalOrLogicalExpressionToken):
+        key = {hash(token.__str__())}
+        if token.value[0].value[0].__class__ == IdentifierToken:
+            key.add(token.value[0].value[0].value)
+
+        if len(token.value) == 3 and token.value[2].value[0].__class__ == IdentifierToken:
+            key.add(token.value[2].value[0].value)
+
+        key = frozenset(key)
+        if self.arithm_cache.get(key) is not None:
+            print(f'Тут есть кэш для {key}')
+            return self.arithm_cache[key]
+
         operations = {
             ArithmeticalSymbolToken.__name__: lambda a, b, operator: a + b if operator == '+' else a - b,
             OrToken.__name__: lambda a, b, _: a or b,
@@ -54,11 +76,15 @@ class Interpreter:
             ShlToken.__name__: lambda a, b, _: a << b,
         }
         if len(token.value) == 3:
-            return operations[token.value[1].value[0].__class__.__name__](self.get_operand_value(token.value[0]),
-                                                                 self.get_operand_value(token.value[2]),
-                                                                 token.value[1].value[0].value)
+            result = operations[token.value[1].value[0].__class__.__name__](self.get_operand_value(token.value[0]),
+                                                                            self.get_operand_value(token.value[2]),
+                                                                            token.value[1].value[0].value)
         else:
-            return self.get_operand_value(token.value[0])
+            result = self.get_operand_value(token.value[0])
+
+        self.arithm_cache[key] = result
+        print(f'Назначили кэш для {key}')
+        return result
 
     def get_cases_by_case_list_token(self, case_list: CaseListToken):
         if case_list.value[0].__class__ == CaseListToken:
@@ -87,6 +113,7 @@ class Interpreter:
             for i in token.value[2].get_all_identifiers():
                 print(hex(self.get_var(i.value)))
         elif token.__class__ == EqualOperatorToken:
+            self.drop_cache(token.value[0].value)
             self.set_var(token.value[0].value, self.arithmetical_or_logical_expression_exec(token.value[2]))
         elif token.__class__ == CaseConstructionToken:
             identifier = token.value[1].value
